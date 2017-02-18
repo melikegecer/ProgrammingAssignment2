53580531f2be0115dab14c912fea49be51c903cd
melikegecer <melikegecer@users.noreply.github.com>
2017-02-18 21:29:27 +0200

    Updated with my own answers

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+## In this assigment, I am trying to cache an inverse of a matrix.
+## Assuming that given matrix is invertable.
 
-## Write a short comment describing this function
-
+## this function returns a vector which contains some functions explained below
+## setMatrix - changes value of a matrix with given new value 
+## getMatrix - returns value of a matrix
+## setInverse - changes value of an inverse of a matrix 
+## getInverse - returns inverse value of a matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## declaration for inverse matrix
+  inverseOfMatrix <- NULL
+  
+  setMatrix <- function(newValue) {
+    x <<- newValue
+    inverseOfMatrix <<- NULL
+  }
+  
+  getMatrix <- function()
+    x
+  
+  setInverse <- function(newValue)
+    inverseOfMatrix <<- newValue
+  
+  getInverse <- function()
+    inverseOfMatrix
+  
+  ## return all functions defined above
+  list(
+    setMatrix = setMatrix,
+    getMatrix = getMatrix,
+    setInverse = setInverse,
+    getInverse = getInverse
+  )
 }
 
 
-## Write a short comment describing this function
+## solve(x) - gives the inverse of a square matrix
+## check for more information    ?solve
 
+## solve cache and return inverse matrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  # let us keep inverse of a matrix
+  inverseOfMatrix <- x$getInverse()
+  
+  ## matrix should be checked if it is empty or not
+  if (!is.null(inverseOfMatrix)) {
+    message('get chached inversed matrix')
+    
+  } else {
+    copy <- x$getMatrix()
+    inverseOfMatrix <- solve(copy, ...)
+    x$setInverse(inverseOfMatrix)
+  }
+  inverseOfMatrix
 }
